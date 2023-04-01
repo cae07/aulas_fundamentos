ca7b50322b9bccf07540c0a75e0ffdfbfe2cacf8
cae <eduardokae01@hotmail.com>
2023-04-01 14:09:06 -0300

    aula operacoes aritmeticas

---
+++ script.js
@@ -0,0 +1,28 @@
+// sum => +
+// subtraction => -
+// division => /
+// multiplications => *
+// exponenciação => **
+
+const sum = 12 + 7;
+const subtraction = 52 - 17;
+const division = 84 / 6;
+const multiplications = 5 * 3;
+const exponenciação = 3 ** 4
+
+// console.log("sum => ", sum);
+// console.log("subtraction => ", subtraction);
+// console.log("division => ", division);
+// console.log("multiplications => ", multiplications);
+// console.log("exponenciação => ", exponenciação);
+
+let salario = 3000;
+
+salario = salario + 500;
+salario = salario * 2;
+
+// resto => %
+
+const isOdd = 401 % 2 === 0;
+
+console.log("é par? = ", isOdd);
