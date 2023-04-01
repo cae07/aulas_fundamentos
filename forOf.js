84bdc10c5a6f3a3db5b2f6bf9fb6fd6e9b14b908
cae <eduardokae01@hotmail.com>
2023-04-01 16:33:30 -0300

    aula loop

---
+++ forOf.js
@@ -0,0 +1,18 @@
+// const listaDeCompras = ['Carne', 'Linguiça', 'Pão de alho', 'Refrigerante', 'Carvão'];
+
+// for (let alimento of listaDeCompras) {
+//   console.log(alimento);
+//   if (alimento === 'Pão de alho') {
+//     console.log('apenas pão de alho aqui');
+//   }
+// }
+
+const greetings = 'Hello World';
+
+// for (let index = 0; index < greetings.length; index += 1) {
+//   console.log(greetings[index])
+// }
+
+for (let letter of greetings) {
+  console.log(letter);
+}
---
+++ loopFor.js
@@ -0,0 +1,7 @@
+const listaDeCompras = ['Carne', 'Linguiça', 'Pão de alho', 'Refrigerante', 'Carvão'];
+
+listaDeCompras.push('Sobremesa');
+
+for (let index = 0; index < listaDeCompras.length; index += 1) {
+  console.log(listaDeCompras[index]);
+};
