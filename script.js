b411dae080d55f25934d58ab0b3bdea747bc338c
cae <eduardokae01@hotmail.com>
2023-04-01 13:57:02 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,8 @@
+// var -> mutável / hoisting => ES6 -> ecmaScript 6
+// let -> mutável
+// const -> imutável
+
+const age = 42;
+
+age = '18'
+console.log(typeof age)
---
+++ tiposPrimitivos.js
@@ -0,0 +1,12 @@
+let number = 18;
+let string = "qualquer escrita com @#$@#%$ qualquer caracter e até 239872345"
+let boolean = true;
+boolean = false;
+let firstUndefined = undefined;
+let second = null;
+
+let arrays = [];
+let objects = {};
+
+console.log("arrays => ", typeof arrays);
+console.log("objects => ", typeof objects);
