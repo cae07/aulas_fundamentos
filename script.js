315e583145bef60db8abdd530793d9db31a7a6d7
cae <eduardokae01@hotmail.com>
2023-04-02 11:16:05 -0300

    aula op ternario

---
+++ script.js
@@ -0,0 +1,11 @@
+const isTrue = 3 + 5 === 7 ? true : false;
+
+// informação ? isso é verdade : isso é mentira;
+
+// console.log(isTrue);
+
+const verifySum = (valueA, valueB) =>
+  valueA + valueB === 7 ? 'isso é verdade' : 'isso é mentira';
+
+
+console.log(verifySum(3, 5));
