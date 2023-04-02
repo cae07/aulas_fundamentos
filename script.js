259f71c69589e76cb5af18ff4eea58d7207301db
cae <eduardokae01@hotmail.com>
2023-04-02 15:26:07 -0300

    aula criando elementos

---
+++ script.js
@@ -0,0 +1,19 @@
+const ingredientsList = [
+  '2 xícaras (chá) de açúcar',
+  '3 xícaras (chá) de farinha de trigo',
+  '4 colheres (sopa) de margarina',
+  '3 ovos',
+  '1 e 1/2 xícara (chá) de leite',
+  '1 colher (sopa) bem cheia de fermento em pó'
+];
+
+const listToAdd = document.querySelector('.unordered_list');
+
+for (let index = 0; index < ingredientsList.length; index += 1) {
+  const ingredient = ingredientsList[index];
+
+  const newList = document.createElement('li');
+  newList.innerText = ingredient;
+  
+  listToAdd.appendChild(newList);
+}
