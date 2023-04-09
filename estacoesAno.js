876e493a6769f4afe603eb554425ac9e7a2b1767
cae <eduardokae01@hotmail.com>
2023-04-09 14:03:43 -0300

    aula spread

---
+++ estacoesAno.js
@@ -0,0 +1,14 @@
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const estacoesDoAno = [
+  ...spring,
+  ...summer,
+  ...fall,
+  ...winter
+]
+
+// console.log(`Os meses do ano são ${estacoesDoAno}`);
+console.log(estacoesDoAno);
---
+++ index.js
@@ -0,0 +1,13 @@
+const shoppingCart = [
+  'videogame',
+  'televisor',
+  'tenis',
+  'camisetas',
+  'calças'
+]
+
+const cartClone = [...shoppingCart, 'prateleiras'];
+
+console.log(shoppingCart);
+console.log('@@@@@@@@@@@@@@');
+console.log(cartClone);
